8959ad73e52d68d2dbbf0cdd1ec3f22feeb60efb
github1320 <32762746+github1320@users.noreply.github.com>
2017-12-26 13:51:49 +0300

    Update MATRIXx(1).cpp

--- MATRIXx(1).cpp
+++ MATRIXx(1).cpp
@@ -8,10 +8,10 @@
 #include <cmath>
 using namespace std;
 
-class matrix
+class matri
 {
 int **m_array;
-int m,n; //m-строки, n-столбцы
+int m,n; //m-Г±ГІГ°Г®ГЄГЁ, n-Г±ГІГ®Г«ГЎГ¶Г»
 
 public:
 friend matrix operator*(matrix A,matrix B);
@@ -20,7 +20,7 @@ friend matrix operator*(matrix A,double x);
 friend ostream &operator<<(ostream &stream,matrix C);
 friend double determine(matrix A);
 friend matrix tran(matrix A);
-matrix& operator=(matrix I)//перегрузка
+matrix& operator=(matrix I)//ГЇГҐГ°ГҐГЈГ°ГіГ§ГЄГ 
 {
   m_array = new int* [I.m];
   for (int count = 0; count < I.m; count++)
@@ -78,7 +78,7 @@ int h;
 int i=-1;
 while (i<m-1)
 { i++;
-cout<<"Введите элементы "<<i+1<<" строки:"<<endl;
+cout<<"Г‚ГўГҐГ¤ГЁГІГҐ ГЅГ«ГҐГ¬ГҐГ­ГІГ» "<<i+1<<" Г±ГІГ°Г®ГЄГЁ:"<<endl;
 for (int j=0;j<n;j++)
 { h=1;u=0;
     cin>>a;
@@ -88,11 +88,11 @@ for (int j=0;j<n;j++)
     u=u*10+(int)a[k]-48;}
 else {if (j==0)
     {
-        i=i-1;j=n;cout<<"ошибка ввода"<<endl;break;
+        i=i-1;j=n;cout<<"Г®ГёГЁГЎГЄГ  ГўГўГ®Г¤Г "<<endl;break;
     }
     else
 
-    {j=j-1;cout<<"ошибка ввода"<<endl;break;}}
+    {j=j-1;cout<<"Г®ГёГЁГЎГЄГ  ГўГўГ®Г¤Г "<<endl;break;}}
     m_array[i][j]=u;
     }
 }
@@ -104,7 +104,7 @@ double determine()
     if (n==m) {
 int size=n;
 return determ(m_array,size);}
-else {cout<<"Ошибка.Матрица не квадратная!"<<endl;return 0; }
+else {cout<<"ГЋГёГЁГЎГЄГ .ГЊГ ГІГ°ГЁГ¶Г  Г­ГҐ ГЄГўГ Г¤Г°Г ГІГ­Г Гї!"<<endl;return 0; }
 }
 matrix tran()
 {int i,j;
@@ -139,7 +139,7 @@ for (int j=0;j<B.n;j++)
 {
 C.m_array[i][j]=A.m_array[i][j]+B.m_array[i][j];
 }}}
-else {cout<<"Oшибка.Матрицы разного размера!"<<endl;return C;}
+else {cout<<"OГёГЁГЎГЄГ .ГЊГ ГІГ°ГЁГ¶Г» Г°Г Г§Г­Г®ГЈГ® Г°Г Г§Г¬ГҐГ°Г !"<<endl;return C;}
 return C;
 }
 
@@ -149,7 +149,7 @@ return C;
 void show_all_matrix(map<char,matrix> all_matrix)
 {
     map<char,matrix>::iterator it = all_matrix.begin() ;
-    for (  it = all_matrix.begin(); it != all_matrix.end(); ++it)///вывод на экран
+    for (  it = all_matrix.begin(); it != all_matrix.end(); ++it)///ГўГ»ГўГ®Г¤ Г­Г  ГЅГЄГ°Г Г­
   {
      cout << it->first << " : \n"<< it->second << endl;
   }
@@ -178,7 +178,7 @@ C.m_array[i][j]+=A.m_array[i][k]*B.m_array[k][j];
 
 }
 return C;}
-cout<<"Ошибка.Не выполняется условие перемножение матриц!"<<endl;
+cout<<"ГЋГёГЁГЎГЄГ .ГЌГҐ ГўГ»ГЇГ®Г«Г­ГїГҐГІГ±Гї ГіГ±Г«Г®ГўГЁГҐ ГЇГҐГ°ГҐГ¬Г­Г®Г¦ГҐГ­ГЁГҐ Г¬Г ГІГ°ГЁГ¶!"<<endl;
 return B;
 }
 matrix operator*(matrix A,double x)
@@ -219,7 +219,7 @@ matrix L(2,2);
 while(atoi(i)!=8)
 {
 
-cout<<"Выберите операцию:  1-транспонирование, 2-определитель"<<endl<<"3-умножение на число, 4-сложить матрицы, 5-перемножить матрицы, 6-добавить матрицу, 7-показать все матрицы, 8-выход"<<endl;
+cout<<"Г‚Г»ГЎГҐГ°ГЁГІГҐ Г®ГЇГҐГ°Г Г¶ГЁГѕ:  1-ГІГ°Г Г­Г±ГЇГ®Г­ГЁГ°Г®ГўГ Г­ГЁГҐ, 2-Г®ГЇГ°ГҐГ¤ГҐГ«ГЁГІГҐГ«Гј"<<endl<<"3-ГіГ¬Г­Г®Г¦ГҐГ­ГЁГҐ Г­Г  Г·ГЁГ±Г«Г®, 4-Г±Г«Г®Г¦ГЁГІГј Г¬Г ГІГ°ГЁГ¶Г», 5-ГЇГҐГ°ГҐГ¬Г­Г®Г¦ГЁГІГј Г¬Г ГІГ°ГЁГ¶Г», 6-Г¤Г®ГЎГ ГўГЁГІГј Г¬Г ГІГ°ГЁГ¶Гі, 7-ГЇГ®ГЄГ Г§Г ГІГј ГўГ±ГҐ Г¬Г ГІГ°ГЁГ¶Г», 8-ГўГ»ГµГ®Г¤"<<endl;
 cin>>i;
 
 
@@ -234,7 +234,7 @@ cin>>a;
     cout<<L;
     }
     catch (const out_of_range& oor) {
-    cerr << "Oшибка ввода. Возмоно такой матрицы нет :(" << '\n';
+    cerr << "OГёГЁГЎГЄГ  ГўГўГ®Г¤Г . Г‚Г®Г§Г¬Г®Г­Г® ГІГ ГЄГ®Г© Г¬Г ГІГ°ГЁГ¶Г» Г­ГҐГІ :(" << '\n';
   }
 
 
@@ -249,16 +249,16 @@ cin>>a;
 
     }
     catch (const out_of_range& oor) {
-    cerr << "Oшибка ввода. Возмоно такой матрицы нет :(" << '\n';
+    cerr << "OГёГЁГЎГЄГ  ГўГўГ®Г¤Г . Г‚Г®Г§Г¬Г®Г­Г® ГІГ ГЄГ®Г© Г¬Г ГІГ°ГЁГ¶Г» Г­ГҐГІ :(" << '\n';
   }
 
 break;
 }
 case 3:{
-    cout<<"Выберите матрицу: ";
+    cout<<"Г‚Г»ГЎГҐГ°ГЁГІГҐ Г¬Г ГІГ°ГЁГ¶Гі: ";
     char a;
     cin>>a;
-    cout<<"Введите число: ";
+    cout<<"Г‚ГўГҐГ¤ГЁГІГҐ Г·ГЁГ±Г«Г®: ";
     double x;
     cin>>x;
     try{L=all_matrix.at(a)*x;
@@ -267,13 +267,13 @@ case 3:{
     cout<<L;
     }
     catch (const out_of_range& oor) {
-    cerr << "Oшибка ввода. Возмоно такой матрицы нет :(" << '\n';
+    cerr << "OГёГЁГЎГЄГ  ГўГўГ®Г¤Г . Г‚Г®Г§Г¬Г®Г­Г® ГІГ ГЄГ®Г© Г¬Г ГІГ°ГЁГ¶Г» Г­ГҐГІ :(" << '\n';
   }
     break;
 }
 
 case 4:{
-    cout<<"Выберите матрицы: ";
+    cout<<"Г‚Г»ГЎГҐГ°ГЁГІГҐ Г¬Г ГІГ°ГЁГ¶Г»: ";
     char a,b;
     cin>>a;
     cin>>b;
@@ -283,14 +283,14 @@ case 4:{
     cout<<L;
     }
     catch (const out_of_range& oor) {
-    cerr << "Oшибка ввода. Возмоно такой матрицы нет :(" << '\n';
+    cerr << "OГёГЁГЎГЄГ  ГўГўГ®Г¤Г . Г‚Г®Г§Г¬Г®Г­Г® ГІГ ГЄГ®Г© Г¬Г ГІГ°ГЁГ¶Г» Г­ГҐГІ :(" << '\n';
   }
     break;
 }
 
 
 case 5:{
-    cout<<"Выберите матрицы: ";
+    cout<<"Г‚Г»ГЎГҐГ°ГЁГІГҐ Г¬Г ГІГ°ГЁГ¶Г»: ";
     char a,b;
     cin>>a;
     cin>>b;
@@ -300,43 +300,43 @@ case 5:{
     cout<<L;
     }
     catch (const out_of_range& oor) {
-    cerr << "Oшибка ввода. Возмоно такой матрицы нет :(" << '\n';
+    cerr << "OГёГЁГЎГЄГ  ГўГўГ®Г¤Г . Г‚Г®Г§Г¬Г®Г­Г® ГІГ ГЄГ®Г© Г¬Г ГІГ°ГЁГ¶Г» Г­ГҐГІ :(" << '\n';
 
   }break;}
 case 6:
     {
      q=0;
-     cout<<"Введите литеру матрицы: ";
+     cout<<"Г‚ГўГҐГ¤ГЁГІГҐ Г«ГЁГІГҐГ°Гі Г¬Г ГІГ°ГЁГ¶Г»: ";
      cin>>name;
-     while (q==0) if ((int)name==76) {cout<<"это название использовать нельзя"<<endl;cin>>name;} else {q=1;}
+     while (q==0) if ((int)name==76) {cout<<"ГЅГІГ® Г­Г Г§ГўГ Г­ГЁГҐ ГЁГ±ГЇГ®Г«ГјГ§Г®ГўГ ГІГј Г­ГҐГ«ГјГ§Гї"<<endl;cin>>name;} else {q=1;}
      try
      {
          cout<<all_matrix.at(name)<<endl;
-         cout<<"Такая матрица уже есть. Переписать?  <Y/N> "<<endl;
+         cout<<"Г’Г ГЄГ Гї Г¬Г ГІГ°ГЁГ¶Г  ГіГ¦ГҐ ГҐГ±ГІГј. ГЏГҐГ°ГҐГЇГЁГ±Г ГІГј?  <Y/N> "<<endl;
          char ans[255];
          cin>>ans;
          if(ans[0]=='Y')
             {
-                cout<<"Заполним матрицу "<<name<<": "<<endl;
-                cout<<"Количество строк: ";
+                cout<<"Г‡Г ГЇГ®Г«Г­ГЁГ¬ Г¬Г ГІГ°ГЁГ¶Гі "<<name<<": "<<endl;
+                cout<<"ГЉГ®Г«ГЁГ·ГҐГ±ГІГўГ® Г±ГІГ°Г®ГЄ: ";
                 cin>>m;
-                cout<<"Количество столбцов: ";
+                cout<<"ГЉГ®Г«ГЁГ·ГҐГ±ГІГўГ® Г±ГІГ®Г«ГЎГ¶Г®Гў: ";
                 cin>>n;
                 matrix A(m,n);
                 A.fill_matrix();
                 all_matrix.at(name)=A;
             }
          else if(ans[0]=='N') {break;}
-         else {cout<<"Ошибка"<<endl;break;}
+         else {cout<<"ГЋГёГЁГЎГЄГ "<<endl;break;}
 
 
     }
     catch (const out_of_range& oor)
     {
-      cout<<"Заполним матрицу "<<name<<": "<<endl;
-      cout<<"Количество строк: ";
+      cout<<"Г‡Г ГЇГ®Г«Г­ГЁГ¬ Г¬Г ГІГ°ГЁГ¶Гі "<<name<<": "<<endl;
+      cout<<"ГЉГ®Г«ГЁГ·ГҐГ±ГІГўГ® Г±ГІГ°Г®ГЄ: ";
       cin>>m;
-      cout<<"Количество столбцов: ";
+      cout<<"ГЉГ®Г«ГЁГ·ГҐГ±ГІГўГ® Г±ГІГ®Г«ГЎГ¶Г®Гў: ";
       cin>>n;
       matrix A(m,n);
       A.fill_matrix();
@@ -349,8 +349,8 @@ case 7:
       show_all_matrix(all_matrix);
       break;
     }
-case 8:{cout<<"До свидания.";break;}
-default: {cout<<"Ошибка ввода. Попробуйте ещё раз "<<endl;break;}
+case 8:{cout<<"Г„Г® Г±ГўГЁГ¤Г Г­ГЁГї.";break;}
+default: {cout<<"ГЋГёГЁГЎГЄГ  ГўГўГ®Г¤Г . ГЏГ®ГЇГ°Г®ГЎГіГ©ГІГҐ ГҐГ№Вё Г°Г Г§ "<<endl;break;}
 }
 }
 
